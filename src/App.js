c79a9e4a5a60af7709f8fef83abfb22ad09fa074
shpark7576 <shpark7576@gmail.com>
2023-05-03 14:13:35 +0900

    customer js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 
 import './App.css';
 import React ,{ Component } from 'react';
+import Customer from './compoments/Customer';
+
+const customers = [{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '홍길동',
+  'birthday' : '961222',
+  'gender' :'남자'
+}
+,
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/any/2',
+  'name' : '홍동2',
+  'birthday' : '961222',
+  'gender' :'남자'
+}
+,{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/any/3',
+  'name' : '홍길3',
+  'birthday' : '961222',
+  'gender' :'남자'
+}
+]
 
 class App extends Component {
   render() {
     return (
              <div className='gray-background'> 
-              aaa
+              <Customer/>
               </div>
     );
       
---
+++ src/compoments/Customer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Customer extends React.Component{
+    render(){
+        return(
+            <div>aaa</div>
+        );
+            
+        
+    }
+}
+
+
+export default Customer;
